eb27c6b0a41b31c35e8f5dfe4fce7638cade6714
Zac Allen <zac.allen@outlook.com>
2019-09-09 11:32:28 +0000

    edited map sizing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 function initMap() {
   // The location of Cork
-  var cork = {lat: 51.8985, lng: 8.4756};
+  var cork = {lat: 51.8985, lng: -8.4756};
   // The map, centered at Cork
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: cork});
+      document.getElementById('map'), {zoom: 8, center: cork});
   // The marker, positioned at Cork
   var marker = new google.maps.Marker({position: cork, map: map});
 }
