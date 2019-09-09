4905e49c21006dddeedf05fd6748af9116ffb3c7
Zac Allen <zac.allen@outlook.com>
2019-09-09 13:27:08 +0000

    added marker cluster to maps for cork comedy clubs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,41 @@
 function initMap() {
-  // The location of Ireland
-  var ireland = {lat:53.1424 , lng: -7.6921 };
-  // The location of Counties
-  var cork = {lat: 51.8985, lng: -8.4756};
-  var dublin = {lat: 53.3498, lng: -6.2603};
-  var limerick = {lat:  52.6638, lng: -8.6267};
-  
-  // The map, centered at Cork
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 6, center: ireland});
-  // The marker, positioned at Cork
-  var marker = new google.maps.Marker({position: cork, map: map});
-  var marker = new google.maps.Marker({position: dublin, map: map});
-  var marker = new google.maps.Marker({position: limerick, map: map});
-}
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 6,
+          center: {lat:53.1424 , lng: -7.6921 }
+        });
+
+
+        var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        var markers = locations.map(function(location, i) {
+          return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+          });
+        });
+
+        // Add a marker clusterer to manage the markers.
+        var markerCluster = new MarkerClusterer(map, markers,
+            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+      }
+      
+      var locations = [
+        {lat: 51.901923, lng: -8.470862},
+        {lat: 51.892656, lng: -8.468170},
+        {lat: 51.898848, lng: -8.476154},
+        {lat: 51.898224, lng: -8.481427},
+        ]
+
+
+/*
+postions of comedy clubs
+CORK
+city limits cork 51.901923, -8.470862
+coughlans comedy club 51.892656, -8.468170
+the roundy 51.898848, -8.476154
+the wonky donkey 51.898224, -8.481427
+
+DUBLIN
+
+*/
