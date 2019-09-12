8f910d6bd20526dc1bbbbf658d6b8caab4a565d4
Zac Allen <zac.allen@outlook.com>
2019-09-12 10:41:33 +0000

    stlyed layout for results box cork - same layout to be used for other counties

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,57 @@
 $(document).ready(function() {
-  
-  $('#cork').click(function(){
-    $('.corkResults').slideDown('slow');
-    $('.resultsBox').slideDown('1800')
-      });
 
-  $('button').click(function(){
-    
-    $(this).next().slideToggle('slow');
-    
+// Selector Buttons
+
+
+  $('#cork').click(function() {
+    $('.dublinResults').slideUp('slow');
+    $('.limerickResults').slideUp('slow');
+    $('.corkResults').slideDown('slow');
+    $('.corkResultsBox').slideDown('1800');
   });
-    
+
+  $('#dublin').click(function() {
+    $('.corkResults').slideUp('slow');
+    $('.limerickResults').slideUp('slow');
+    $('.dublinResults').slideDown('slow');
+    $('.resultsBox').slideDown('1800')
   });
+
+  $('#limerick').click(function() {
+    $('.dublinResults').slideUp('slow');
+    $('.corkResults').slideUp('slow');
+    $('.limerickResults').slideDown('slow');
+    $('.resultsBox').slideDown('1800')
+  });
+  
+//Club Buttons
+
+  $('#cityLimitsButton').click(function() {
+    $('#comedyCavernContent').slideUp('slow');
+    $('#cocoClubContent').slideUp('slow');
+    $('#wonkyDonkeyContent').slideUp('slow');
+    $('#cityLimitsContent').slideToggle('slow');
+  });
+
+  $('#comedyCavernButton').click(function() {
+    $('#cityLimitsContent').slideUp('slow');
+    $('#cocoClubContent').slideUp('slow');
+    $('#wonkyDonkeyContent').slideUp('slow');
+    $('#comedyCavernContent').slideToggle('slow');
+  });
+  
+  $('#cocoClubButton').click(function() {
+    $('#cityLimitsContent').slideUp('slow');
+    $('#comedyCavernContent').slideUp('slow');
+    $('#wonkyDonkeyContent').slideUp('slow');
+    $('#cocoClubContent').slideToggle('slow');
+  });
+  
+  $('#wonkyDonkeyButton').click(function() {
+    $('#cityLimitsContent').slideUp('slow');
+    $('#cocoClubContent').slideUp('slow');
+    $('#comedyCavernContent').slideUp('slow');
+    $('#wonkyDonkeyContent').slideToggle('slow');
+  });
+
+});
