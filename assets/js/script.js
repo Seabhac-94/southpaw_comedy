33d88f8455cd6de05fe4d9134cf3e642ffc4c984
Zac Allen <zac.allen@outlook.com>
2019-09-16 15:35:32 +0000

    reduced code size in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,30 +4,19 @@ $(document).ready(function() {
 
 
   $('#cork').click(function() {
-    $('.dublinResults').slideUp('medium');
-    $('.limerickResults').slideUp('medium');
-    $('.dublinResultsBox').slideUp('medium');
-    $('.limerickResultsBox').slideUp('medium');
+    $('.dublinResults, .limerickResults, .dublinResultsBox, .limerickResultsBox').slideUp('medium');
     $('.corkResults').slideDown('slow');
     $('.corkResultsBox').slideDown(1800);
   });
-  
-
 
   $('#dublin').click(function() {
-    $('.corkResults').slideUp('medium');
-    $('.limerickResults').slideUp('medium');
-    $('.corkResultsBox').slideUp('medium');
-    $('.limerickResultsBox').slideUp('medium');
+    $('.corkResults, .limerickResults, .corkResultsBox, .limerickResultsBox').slideUp('medium');
     $('.dublinResults').slideDown('slow');
     $('.dublinResultsBox').slideDown(1800)
   });
 
   $('#limerick').click(function() {
-    $('.dublinResults').slideUp('medium');
-    $('.corkResults').slideUp('medium');
-    $('.dublinResultsBox').slideUp('medium');
-    $('.corkResultsBox').slideUp('medium');
+    $('.dublinResults, .corkResults, .dublinResultsBox, corkResultsBox').slideUp('medium');
     $('.limerickResults').slideDown('slow');
     $('.limerickResultsBox').slideDown(1800)
   });
@@ -35,62 +24,45 @@ $(document).ready(function() {
   //Cork Club Buttons
 
   $('#cityLimitsButton').click(function() {
-    $('#comedyCavernContent').slideUp('slow');
-    $('#cocoClubContent').slideUp('slow');
-    $('#wonkyDonkeyContent').slideUp('slow');
+    $('#comedyCavernContent, #cocoClubContent, #wonkyDonkeyContent').slideUp('slow');
     $('#cityLimitsContent').slideToggle('slow');
   });
 
   $('#comedyCavernButton').click(function() {
-    $('#cityLimitsContent').slideUp('slow');
-    $('#cocoClubContent').slideUp('slow');
-    $('#wonkyDonkeyContent').slideUp('slow');
+    $('#cityLimitsContent, #cocoClubContent, #wonkyDonkeyContent').slideUp('slow');
     $('#comedyCavernContent').slideToggle('slow');
   });
 
   $('#cocoClubButton').click(function() {
-    $('#cityLimitsContent').slideUp('slow');
-    $('#comedyCavernContent').slideUp('slow');
-    $('#wonkyDonkeyContent').slideUp('slow');
+    $('#cityLimitsContent, #comedyCavernContent, #wonkyDonkeyContent').slideUp('slow');
     $('#cocoClubContent').slideToggle('slow');
   });
 
   $('#wonkyDonkeyButton').click(function() {
-    $('#cityLimitsContent').slideUp('slow');
-    $('#cocoClubContent').slideUp('slow');
-    $('#comedyCavernContent').slideUp('slow');
+    $('#cityLimitsContent, #cocoClubContent, #comedyCavernContent').slideUp('slow');
     $('#wonkyDonkeyContent').slideToggle('slow');
   });
-
 
 
   //Dublin Club Buttons
 
   $('#laughterLoungeButton').click(function() {
-    $('#comedyCellarContent').slideUp('slow');
-    $('#jestersComedyContent').slideUp('slow');
-    $('#popCherryContent').slideUp('slow');
+    $('#comedyCellarContent, #jestersComedyContent, #popCherryContent').slideUp('slow');
     $('#laughterLoungeContent').slideToggle('slow');
   });
 
   $('#comedyCellarButton').click(function() {
-    $('#laughterLoungeContent').slideUp('slow');
-    $('#jestersComedyContent').slideUp('slow');
-    $('#popCherryContent').slideUp('slow');
+    $('#laughterLoungeContent, #jestersComedyContent, #popCherryContent').slideUp('slow');
     $('#comedyCellarContent').slideToggle('slow');
   });
 
   $('#jestersComedyButton').click(function() {
-    $('#comedyCellarContent').slideUp('slow');
-    $('#laughterLoungeContent').slideUp('slow');
-    $('#popCherryContent').slideUp('slow');
+    $('#comedyCellarContent, #laughterLoungeContent, #popCherryContent').slideUp('slow');
     $('#jestersComedyContent').slideToggle('slow');
   });
 
   $('#popCherryButton').click(function() {
-    $('#jestersComedyContent').slideUp('slow');
-    $('#comedyCellarContent').slideUp('slow');
-    $('#laughterLoungeContent').slideUp('slow');
+    $('#jestersComedyContent, #comedyCellarContent, #laughterLoungeContent').slideUp('slow');
     $('#popCherryContent').slideToggle('slow');
   });
 
@@ -98,28 +70,26 @@ $(document).ready(function() {
   //Limerick Club Buttons
 
   $('#treatyComedyButton').click(function() {
-    $('#filibusterContent').slideUp('slow');
-    $('#karlnivalContent').slideUp('slow');
+    $('#filibusterContent, #karlnivalContent').slideUp('slow');
     $('#treatyComedyContent').slideToggle('slow');
   });
 
   $('#filibusterButton').click(function() {
-    $('#treatyComedyContent').slideUp('slow');
-    $('#karlnivalContent').slideUp('slow');
+    $('#treatyComedyContent, #karlnivalContent').slideUp('slow');
     $('#filibusterContent').slideToggle('slow');
   });
 
   $('#karlnivalButton').click(function() {
-    $('#filibusterContent').slideUp('slow');
-    $('#treatyComedyContent').slideUp('slow');
+    $('#filibusterContent, #treatyComedyContent').slideUp('slow');
     $('#karlnivalContent').slideToggle('slow');
   });
 
 
-  $('#cork, #dublin, #limerick').click(function(){
+  // Opacity
+
+  $('#cork, #dublin, #limerick').click(function() {
     $('.mapOpacity').css('opacity', '1')
   });
 
- 
 
 });
