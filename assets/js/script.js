780e2b7bf541abbf43aefa7fcdf8318c8a37c154
Zac Allen <zac.allen@outlook.com>
2019-09-16 16:06:52 +0000

    passed .reset-function through script.js to allow all paragraphs to reset when county button is pressed

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,19 +4,19 @@ $(document).ready(function() {
 
 
   $('#cork').click(function() {
-    $('.dublinResults, .limerickResults, .dublinResultsBox, .limerickResultsBox').slideUp('medium');
+    $('.dublinResults, .limerickResults, .dublinResultsBox, .limerickResultsBox, .reset-function').slideUp('medium');
     $('.corkResults').slideDown('slow');
     $('.corkResultsBox').slideDown(1800);
   });
 
   $('#dublin').click(function() {
-    $('.corkResults, .limerickResults, .corkResultsBox, .limerickResultsBox').slideUp('medium');
+    $('.corkResults, .limerickResults, .corkResultsBox, .limerickResultsBox, .reset-function').slideUp('medium');
     $('.dublinResults').slideDown('slow');
     $('.dublinResultsBox').slideDown(1800)
   });
 
   $('#limerick').click(function() {
-    $('.dublinResults, .corkResults, .dublinResultsBox, .corkResultsBox').slideUp('medium');
+    $('.dublinResults, .corkResults, .dublinResultsBox, .corkResultsBox, .reset-function').slideUp('medium');
     $('.limerickResults').slideDown('slow');
     $('.limerickResultsBox').slideDown(1800)
   });
